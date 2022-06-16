8057c22a30c9aae7c5252eef222aa2a9a87d9fbf
marcel-noehre <marcelnoehre@web.de>
2022-06-16 23:39:32 +0200

    handle tokens

--- src/app/components/hints/hints.component.ts
+++ src/app/components/hints/hints.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-hints',
@@ -11,38 +12,71 @@ export class HintsComponent implements OnInit {
   secondSelected:boolean = false;
   thirdSelected: boolean = false;
 
-  constructor() { }
+  constructor(
+    private storage: StorageService
+  ) { }
 
   ngOnInit(): void {
   }
 
   firstHint() {
     if(!this.firstSelected) {
-      this.firstSelected = true;
-      this.secondSelected = false;
-      this.thirdSelected = false;
+      if(this.remainingTokens >= 1) {
+        this.remainingTokens = this.remainingTokens -1;
+        this.firstSelected = true;
+        this.secondSelected = false;
+        this.thirdSelected = false;
+      }
     }
   }
 
   secondHint() {
+    let tokens = this.remainingTokens;
     if(!this.secondSelected) {
-      this.firstSelected = true;
-      this.secondSelected = true;
-      this.thirdSelected = false;
+      if(this.firstSelected) {
+        tokens = tokens - 1;
+      } else {
+        tokens = tokens - 2;
+      }
+      if(tokens >= 0) {
+        this.remainingTokens = tokens;
+        this.firstSelected = true;
+        this.secondSelected = true;
+        this.thirdSelected = false;
+      }
     }
   }
 
   thirdHint() {
+    let tokens = this.remainingTokens;
     if(!this.thirdSelected) {
-      this.firstSelected = true;
-      this.secondSelected = true;
-      this.thirdSelected = true;
-    }
+        if(this.secondSelected) {
+          tokens = tokens - 1;
+        } else if(this.firstSelected) {
+          tokens = tokens - 2;
+        } else {
+          tokens = tokens - 3;
+        }
+        if(tokens >= 0) {
+          this.remainingTokens = tokens;
+          this.firstSelected = true;
+          this.secondSelected = true;
+          this.thirdSelected = true;
+        }
+      }
   }
 
   resetHints() {
     this.firstSelected = false;
     this.secondSelected = false;
     this.thirdSelected = false;
+    let diff = this.storage.getSessionEntry('difficulty');
+    if(diff == 'BEGINNER') {
+      this.remainingTokens = 10;
+    } else if (diff == 'ADVANCED') {
+      this.remainingTokens = 15;
+    } else {
+      this.remainingTokens = 20;
+    }
   }
 }
