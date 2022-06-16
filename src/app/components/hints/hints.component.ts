adf6790f3d7686b31fd8e35261c2873e3d2fb3a9
marcel-noehre <marcelnoehre@web.de>
2022-06-16 15:43:05 +0200

    basic hint mobile design

--- src/app/components/hints/hints.component.ts
+++ src/app/components/hints/hints.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hints.component.scss']
 })
 export class HintsComponent implements OnInit {
+  remainingTokens:number = 10; 
+  firstSelected: boolean = false;
+  secondSelected:boolean = false;
+  thirdSelected: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  firstHint() {
+    if(!this.firstSelected) {
+      this.firstSelected = true;
+      this.secondSelected = false;
+      this.thirdSelected = false;
+    }
+  }
+
+  secondHint() {
+    if(!this.secondSelected) {
+      this.firstSelected = true;
+      this.secondSelected = true;
+      this.thirdSelected = false;
+    }
+  }
+
+  thirdHint() {
+    if(!this.thirdSelected) {
+      this.firstSelected = true;
+      this.secondSelected = true;
+      this.thirdSelected = true;
+    }
+  }
+
+  resetHints() {
+    this.firstSelected = false;
+    this.secondSelected = false;
+    this.thirdSelected = false;
+  }
 }
