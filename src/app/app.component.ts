dfad7ac10c4af91c1c9f469b4f8f443d9b656fd6
Jannis Kehrhahn <jannis.kehrhahn@youco-it.com>
2022-07-11 14:17:04 +0200

    Breakpoint handling is now in a seperate service, responisveClass just needs to be emitted now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from './services/storage.service';
 import { ActionService } from './services/action-service';
 import { DifficultyEnum } from './enum/difficulty';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { NONE_TYPE } from '@angular/compiler';
+import { BreakpointService } from './services/breakpoint.service';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +12,13 @@ import { NONE_TYPE } from '@angular/compiler';
 })
 export class AppComponent implements OnInit {
   displayHandbook: boolean = false;
-  
-  display : { [key: string]: any } = {
-    isHandset: false,
-    isHandsetLandscape: false,
-    isHandsetPortrait: false,
-    isLarge: false,
-    isMedium: false,
-    isSmall: false,
-    isTablet: false,
-    isTabletLandscape: false,
-    isTabletPortrait: false,
-    isWeb: false,
-    isWebLandscape: false,
-    isWebPortrait: false,
-    isXLarge: false,
-    isXSmall: false,
-  };
-
-  responsiveClass : string = '';
 
   title = 'minesweeper-cbr';
 
   constructor(
     private storage: StorageService,
     private action: ActionService,
-    private responsive: BreakpointObserver,
+    private breakpoint: BreakpointService
   ) {
     let diff: string = '';
     try {
@@ -51,80 +32,6 @@ export class AppComponent implements OnInit {
     this.action.displayHandbook.subscribe((displayHandbook) => {
       this.displayHandbook = displayHandbook;
     });
-
-    this.responsive.observe([
-      Breakpoints.Handset,
-      Breakpoints.HandsetLandscape,
-      Breakpoints.HandsetPortrait,
-      Breakpoints.Large,
-      Breakpoints.Medium,
-      Breakpoints.Small,
-      Breakpoints.Tablet,
-      Breakpoints.TabletLandscape,
-      Breakpoints.TabletPortrait,
-      Breakpoints.Web,
-      Breakpoints.WebLandscape,
-      Breakpoints.WebPortrait,
-      Breakpoints.XLarge,
-      Breakpoints.XSmall])
-      .subscribe(result => {
-
-        const breakpoints = result.breakpoints;
-
-        if (breakpoints[Breakpoints.Handset]) {
-          this.displayMode("isHandset");
-          this.responsiveClass = "is-handset";
-        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
-          this.displayMode("isHandsetLandscape");
-          this.responsiveClass = "is-handset-landscape";
-        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
-          this.displayMode("isHandsetPortrait");
-          this.responsiveClass = "is-handset-portrait";
-        } else if (breakpoints[Breakpoints.Large]) {
-          this.displayMode("isLarge");
-          this.responsiveClass = "is-large";
-        } else if (breakpoints[Breakpoints.Medium]) {
-          this.displayMode("isMedium");
-          this.responsiveClass = "is-medium";
-        } else if (breakpoints[Breakpoints.Small]) {
-          this.displayMode("isSmall");
-          this.responsiveClass = "is-small";
-        } else if (breakpoints[Breakpoints.Tablet]) {
-          this.displayMode("isTablet");
-          this.responsiveClass = "is-tablet";
-        } else if (breakpoints[Breakpoints.TabletLandscape]) {
-          this.displayMode("isTabletLandscape");
-          this.responsiveClass = "is-tablet-landscape";
-        } else if (breakpoints[Breakpoints.TabletPortrait]) {
-          this.displayMode("isTabletPortrait");
-          this.responsiveClass = "is-tablet-portrait";
-        } else if (breakpoints[Breakpoints.Web]) {
-          this.displayMode("isWeb");
-          this.responsiveClass = "is-web";
-        } else if (breakpoints[Breakpoints.WebLandscape]) {
-          this.displayMode("isWebLandscape");
-          this.responsiveClass = "is-web-landscape";
-        } else if (breakpoints[Breakpoints.WebPortrait]) {
-          this.displayMode("isWebPortrait");
-          this.responsiveClass = "is-web-portrait";
-        } else if (breakpoints[Breakpoints.XLarge]) {
-          this.displayMode("isXLarge");
-          this.responsiveClass = "is-x-large";
-        } else if (breakpoints[Breakpoints.XSmall]) {
-          this.displayMode("isXSmall");
-          this.responsiveClass = "is-x-small";
-        }
-      })
   }
 
-  displayMode(mode: string) {
-    for (let displaymode of Object.keys(this.display)) {
-      if (displaymode == mode) {
-        this.display[displaymode] = true;
-        console.log(displaymode, " = true")
-      } else {
-        this.display[displaymode] = false;
-      }
-    }
-  }
 }
--- src/app/services/breakpoint.service.ts
+++ src/app/services/breakpoint.service.ts
@@ -1,8 +1,106 @@
 import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class BreakpointService {
 
+	display: { [key: string]: any } = {
+		isHandset: false,
+		isHandsetLandscape: false,
+		isHandsetPortrait: false,
+		isLarge: false,
+		isMedium: false,
+		isSmall: false,
+		isTablet: false,
+		isTabletLandscape: false,
+		isTabletPortrait: false,
+		isWeb: false,
+		isWebLandscape: false,
+		isWebPortrait: false,
+		isXLarge: false,
+		isXSmall: false,
+	};
+
+	responsiveClass: string = '';
+
+	constructor(
+		private responsive: BreakpointObserver,
+	) {
+		this.responsive.observe([
+			Breakpoints.Handset,
+			Breakpoints.HandsetLandscape,
+			Breakpoints.HandsetPortrait,
+			Breakpoints.Large,
+			Breakpoints.Medium,
+			Breakpoints.Small,
+			Breakpoints.Tablet,
+			Breakpoints.TabletLandscape,
+			Breakpoints.TabletPortrait,
+			Breakpoints.Web,
+			Breakpoints.WebLandscape,
+			Breakpoints.WebPortrait,
+			Breakpoints.XLarge,
+			Breakpoints.XSmall])
+			.subscribe(result => {
+
+				const breakpoints = result.breakpoints;
+
+				if (breakpoints[Breakpoints.Handset]) {
+					this.displayMode("isHandset");
+					this.responsiveClass = "is-handset";
+				} else if (breakpoints[Breakpoints.HandsetLandscape]) {
+					this.displayMode("isHandsetLandscape");
+					this.responsiveClass = "is-handset-landscape";
+				} else if (breakpoints[Breakpoints.HandsetPortrait]) {
+					this.displayMode("isHandsetPortrait");
+					this.responsiveClass = "is-handset-portrait";
+				} else if (breakpoints[Breakpoints.Large]) {
+					this.displayMode("isLarge");
+					this.responsiveClass = "is-large";
+				} else if (breakpoints[Breakpoints.Medium]) {
+					this.displayMode("isMedium");
+					this.responsiveClass = "is-medium";
+				} else if (breakpoints[Breakpoints.Small]) {
+					this.displayMode("isSmall");
+					this.responsiveClass = "is-small";
+				} else if (breakpoints[Breakpoints.Tablet]) {
+					this.displayMode("isTablet");
+					this.responsiveClass = "is-tablet";
+				} else if (breakpoints[Breakpoints.TabletLandscape]) {
+					this.displayMode("isTabletLandscape");
+					this.responsiveClass = "is-tablet-landscape";
+				} else if (breakpoints[Breakpoints.TabletPortrait]) {
+					this.displayMode("isTabletPortrait");
+					this.responsiveClass = "is-tablet-portrait";
+				} else if (breakpoints[Breakpoints.Web]) {
+					this.displayMode("isWeb");
+					this.responsiveClass = "is-web";
+				} else if (breakpoints[Breakpoints.WebLandscape]) {
+					this.displayMode("isWebLandscape");
+					this.responsiveClass = "is-web-landscape";
+				} else if (breakpoints[Breakpoints.WebPortrait]) {
+					this.displayMode("isWebPortrait");
+					this.responsiveClass = "is-web-portrait";
+				} else if (breakpoints[Breakpoints.XLarge]) {
+					this.displayMode("isXLarge");
+					this.responsiveClass = "is-x-large";
+				} else if (breakpoints[Breakpoints.XSmall]) {
+					this.displayMode("isXSmall");
+					this.responsiveClass = "is-x-small";
+				}
+			})
+	}
+
+	displayMode(mode: string) {
+		for (let displaymode of Object.keys(this.display)) {
+			if (displaymode == mode) {
+				this.display[displaymode] = true;
+				console.log(displaymode, " = true")
+			} else {
+				this.display[displaymode] = false;
+			}
+		}
+	}
 }
