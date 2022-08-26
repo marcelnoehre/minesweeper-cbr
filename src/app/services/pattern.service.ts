18fe435be718d565c6926546569c0a766ed1b7bb
marcel-noehre <marcelnoehre@web.de>
2022-08-26 16:14:44 +0200

    get pattern by index

--- src/app/services/pattern.service.ts
+++ src/app/services/pattern.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { BoardService } from './board.service';
+import { GameStatsService } from './gamestats.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class PatternService {
     private predictableIndexes: number[][] = [];
+    private cellsRevealed!: string[][];
+    private cellsPerRow!: number;
+    private patternOrder: number[][] = [
+        [0,0],[-1,-1],[-1,0],[-1,1],[0,1],[-1,1],[0,1],[-1,1],[-1,0],
+        [-2,-2],[-2,-1],[-2,0],[-2,1],[-2,2],[-1,2],[0,2],[1,2],[2,2],
+        [2,1],[2,0],[2,-1],[2,-2],[1,-2],[0,-2],[-1,-2]
+    ];
+
+    constructor(
+        private _board: BoardService,
+        private _gameStats: GameStatsService
+        ) {
+        _board.cellsRevealed$.subscribe((cellRevealed: string[][]) => {
+            this.cellsRevealed = cellRevealed;
+        });
+        _gameStats.cellsPerRow$.subscribe((cellsPerRow: number) => {
+            this.cellsPerRow = cellsPerRow;
+        });
+    }
 
     getCheckablePattern(revealedBoard: string[][]):void {
         for(let i = 0; i < revealedBoard.length; i++) {
@@ -15,5 +36,19 @@ export class PatternService {
             }
         }
         console.log(this.predictableIndexes);
+    }
+
+    getPatternByIndex(x: number, y: number) {
+        let pattern = '';
+        for(let i = 0; i < 25; i++) {
+            if(x + this.patternOrder[i][0] < 0 || 
+                y + this.patternOrder[i][1] < 0 || 
+                x + this.patternOrder[i][0] >= this.cellsPerRow || 
+                y + this.patternOrder[i][1] >= this.cellsPerRow) {
+                pattern += 'B';
+            } else {
+                pattern += this.cellsRevealed[x + this.patternOrder[i][0]][y + this.patternOrder[i][1]];
+            }
+        }
     }
 }
