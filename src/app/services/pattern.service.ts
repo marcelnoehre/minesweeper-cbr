1281fab745d17b6a61419c5c7b936d9871bc8405
marcel-noehre <marcelnoehre@web.de>
2022-08-26 14:13:21 +0200

    create indexes list

--- src/app/services/pattern.service.ts
+++ src/app/services/pattern.service.ts
@@ -4,14 +4,16 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root'
 })
 export class PatternService {
+    private predictableIndexes: number[][] = [];
 
-    getCheckablePattern(revealedBoard: String[][]):void {
+    getCheckablePattern(revealedBoard: string[][]):void {
         for(let i = 0; i < revealedBoard.length; i++) {
             for(let j = 0; j < revealedBoard[i].length; j++) {
                 if(revealedBoard[i][j] == 'C' || revealedBoard[i][j] == 'F') {
-                    console.log('Value: ', i,j, ' ', revealedBoard[i][j]);
+                    this.predictableIndexes.push([i,j]);
                 }
             }
         }
+        console.log(this.predictableIndexes);
     }
 }
