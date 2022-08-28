2cbcd1c85d6d3c80d98c73f9f5bcf9e5eff8f958
marcel-noehre <marcelnoehre@web.de>
2022-08-28 22:25:03 +0200

    block hints before game start

--- src/app/components/hints/hints.component.ts
+++ src/app/components/hints/hints.component.ts
@@ -3,6 +3,7 @@ import { TokensService } from 'src/app/services/tokens.service';
 import { BreakpointService } from 'src/app/services/breakpoint.service';
 import { BoardService } from 'src/app/services/board.service';
 import { PatternService } from 'src/app/services/pattern.service';
+import { GameStatsService } from 'src/app/services/gamestats.service';
 
 @Component({
   selector: 'app-hints',
@@ -12,6 +13,7 @@ import { PatternService } from 'src/app/services/pattern.service';
 export class HintsComponent implements OnInit {
   responsiveClass!: string;
   remainingTokens!: number;
+  gameRunning!: boolean;
   hintStatus!: number;
   cellsRevealed!: string[][];
   hintText!: string;
@@ -20,6 +22,7 @@ export class HintsComponent implements OnInit {
     private _tokens: TokensService,
     private _breakpoints: BreakpointService,
     private _board: BoardService,
+    private _gameStats: GameStatsService,
     private _pattern: PatternService
   ) { }
 
@@ -29,6 +32,9 @@ export class HintsComponent implements OnInit {
     });
     this._tokens.remainingTokens$.subscribe((remainingTokens: number) => {
       this.remainingTokens = remainingTokens;
+    });
+    this._gameStats.gameRunning$.subscribe((gameRunning: boolean) => {
+      this.gameRunning = gameRunning;
     });
     this._tokens.hintStatus$.subscribe((hintStatus: number) => {
       this.hintStatus = hintStatus;
@@ -42,35 +48,39 @@ export class HintsComponent implements OnInit {
   }
 
   async hintSelected(selectedHint: number) {
-    if(this.hintStatus < selectedHint && this.remainingTokens >= selectedHint-this.hintStatus) {
-      this._tokens.setRemainingTokens(this.remainingTokens-(selectedHint-this.hintStatus));
-      this._tokens.setHintStatus(selectedHint);
-      switch(selectedHint) {
-        case 1: {
-          if(!this._tokens.activeHint) {
-            this._tokens.setActiveHint(true);
-            await this._tokens.setupSolution();
+    if(!this.gameRunning) {
+      this._tokens.setHintText('Vor dem ersten Spielzug können keine Hinweise gakauft werden!');
+    } else {
+      if(this.hintStatus < selectedHint && this.remainingTokens >= selectedHint-this.hintStatus) {
+        this._tokens.setRemainingTokens(this.remainingTokens-(selectedHint-this.hintStatus));
+        this._tokens.setHintStatus(selectedHint);
+        switch(selectedHint) {
+          case 1: {
+            if(!this._tokens.activeHint) {
+              this._tokens.setActiveHint(true);
+              await this._tokens.setupSolution();
+            }
+            break;
           }
-          break;
-        }
-        case 2: {
-          if(!this._tokens.activeHint) {
-            this._tokens.setActiveHint(true);
-            await this._tokens.setupSolution();
+          case 2: {
+            if(!this._tokens.activeHint) {
+              this._tokens.setActiveHint(true);
+              await this._tokens.setupSolution();
+            }
+            if(!this._tokens.activeColorArea) {
+              this._tokens.setActiveColorArea(true);
+              await this._tokens.setupColoredArea();
+            }
+            break;
           }
-          if(!this._tokens.activeColorArea) {
-            this._tokens.setActiveColorArea(true);
-            await this._tokens.setupColoredArea();
+          case 3: {
+            if(!this._tokens.activeHint) {
+              this._tokens.setActiveHint(true);
+              await this._tokens.setupSolution();
+            }
+            this._tokens.turnCell();
+            break;
           }
-          break;
-        }
-        case 3: {
-          if(!this._tokens.activeHint) {
-            this._tokens.setActiveHint(true);
-            await this._tokens.setupSolution();
-          }
-          this._tokens.turnCell();
-          break;
         }
       }
     }
